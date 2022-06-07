c840faac09f6e35b527de05ca70e72cff4b65315
paidforby <grant@equalitie.org>
2022-06-07 15:26:31 -0400

    Add CENO WebExtension as submodule that is packaged with RefBrowser as an asset, need to make sure _locales folder is not ignored in build.gradle, install CenoWebExt when creating runtime engine

---
+++ app/src/main/java/org/mozilla/reference/browser/addons/HttpsByDefaultWebExt.kt
@@ -0,0 +1,32 @@
+package org.mozilla.reference.browser.addons
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import mozilla.components.concept.engine.webextension.WebExtensionRuntime
+import mozilla.components.support.base.log.logger.Logger
+
+/**
+ * Feature to enable website-hotfixing via the Web Compatibility System-Addon.
+ */
+object HttpsByDefaultWebExt {
+    private val logger = Logger("equalitie-ceno")
+
+    internal const val CENO_EXTENSION_ID = "ceno@equalit.ie"
+    internal const val CENO_EXTENSION_URL = "resource://android/assets/addons/ceno/"
+
+    /**
+     * Installs the web extension in the runtime through the WebExtensionRuntime install method
+     */
+    fun install(runtime: WebExtensionRuntime) {
+        runtime.installWebExtension(
+                CENO_EXTENSION_ID, CENO_EXTENSION_URL,
+                onSuccess = {
+                    logger.debug("Installed CENO webextension: ${it.id}")
+                },
+                onError = { ext, throwable ->
+                    logger.error("Failed to install CENO webextension: $ext", throwable)
+                }
+        )
+    }
+}
