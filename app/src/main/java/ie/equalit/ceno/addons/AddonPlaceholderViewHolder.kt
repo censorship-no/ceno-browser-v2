d62bcebfaba27a7d871905bee960e5adc3140c95
paidforby <20746091+paidforby@users.noreply.github.com>
2023-01-25 14:37:12 -0500

    Add a placeholder item to AddonsFragment recycler view which informs user that add-ons are not available without using a toast, fixes #32

---
+++ app/src/main/java/ie/equalit/ceno/addons/AddonPlaceholderViewHolder.kt
@@ -0,0 +1,17 @@
+package ie.equalit.ceno.addons
+
+import android.view.View
+import ie.equalit.ceno.R
+import ie.equalit.ceno.utils.view.CenoViewHolder
+
+class AddonPlaceholderViewHolder(
+        view: View
+) : CenoViewHolder(view) {
+
+    fun bind() = Unit
+
+    companion object {
+        const val LAYOUT_ID = R.layout.addon_placeholder_item
+    }
+}
+
