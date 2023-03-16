e39b15d446f518f3d06857eb935fdaa0c15e7285
paidforby <20746091+paidforby@users.noreply.github.com>
2023-03-16 13:42:57 -0400

    Update Android-Components to 111.0, support breaking changes in ToolbarAutocompleteFeature and MediaSessionService

--- app/src/main/java/ie/equalit/ceno/media/MediaSessionService.kt
+++ app/src/main/java/ie/equalit/ceno/media/MediaSessionService.kt
@@ -7,10 +7,12 @@ package ie.equalit.ceno.media
 import mozilla.components.browser.state.store.BrowserStore
 import mozilla.components.feature.media.service.AbstractMediaSessionService
 import ie.equalit.ceno.ext.components
+import mozilla.components.concept.base.crash.CrashReporting
 
 /**
  * [AbstractMediaSessionService] implementation for injecting [BrowserStore] singleton.
  */
 class MediaSessionService : AbstractMediaSessionService() {
     override val store: BrowserStore by lazy { components.core.store }
+    override val crashReporter: CrashReporting by lazy { components.analytics.crashReporter }
 }
