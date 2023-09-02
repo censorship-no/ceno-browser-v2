c39a45b848f5db4fc3a21ef00d976835f7f86286
taslimoseni <taslim@equalitie.org>
2023-09-02 20:38:22 +0100

    Delete KT files

--- app/src/main/java/ie/equalit/ceno/util_background_runner/model/BackgroundDocument.kt
+++
@@ -1,9 +0,0 @@
-package ie.equalit.ceno.util_background_runner.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class BackgroundDocument(
-    val documentUrl: String?,
-    val url: String?,
-)
--- app/src/main/java/ie/equalit/ceno/util_background_runner/model/BackgroundResponse.kt
+++
@@ -1,9 +0,0 @@
-package ie.equalit.ceno.util_background_runner.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class BackgroundResponse(
-    val statusCode: Int = 400,
-    val url: String = "",
-)
