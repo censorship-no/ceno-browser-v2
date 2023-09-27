3bed5f6f912c6a9b560f0ee3e7b0b7727467dc29
taslimoseni <taslim@equalitie.org>
2023-09-28 00:36:07 +0100

    Commit POC

--- app/src/main/java/ie/equalit/ceno/settings/NetworkSettingsFragment.kt
+++ app/src/main/java/ie/equalit/ceno/settings/NetworkSettingsFragment.kt
@@ -12,8 +12,10 @@ import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.preference.ListPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import ie.equalit.ceno.BuildConfig
 import ie.equalit.ceno.R
 import ie.equalit.ceno.ext.getPreferenceKey
 import mozilla.components.support.ktx.android.view.showKeyboard
@@ -21,12 +23,15 @@ import mozilla.components.support.ktx.kotlin.ifNullOrEmpty
 
 class NetworkSettingsFragment : PreferenceFragmentCompat() {
 
+    private val btSourcesMap = mutableMapOf<String, String>()
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.network_detail_preferences, rootKey)
     }
 
     override fun onResume() {
         super.onResume()
+        setUpBTSources()
         setupPreferences()
         getActionBar().apply{
             show()
@@ -35,6 +40,7 @@ class NetworkSettingsFragment : PreferenceFragmentCompat() {
             setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(requireContext(), R.color.ceno_action_bar)))
         }
     }
+
 
     private fun setupPreferences() {
 
@@ -46,58 +52,66 @@ class NetworkSettingsFragment : PreferenceFragmentCompat() {
         val preferenceUpnpStatus = getPreference(R.string.pref_key_ouinet_upnp_status)
         val extraBootstrapBittorrentKey = requireContext().getPreferenceKey(R.string.pref_key_ouinet_extra_bittorrent_bootstraps)
 
-        val preferenceExtraBitTorrentBootstrap = findPreference<Preference>(extraBootstrapBittorrentKey)
-        preferenceExtraBitTorrentBootstrap?.onPreferenceClickListener = getClickListenerForExtraBitTorrentBootstraps()
-
         preferenceAboutOuinetProtocol?.summary = "${CenoSettings.getOuinetProtocol(requireContext())}"
         preferenceReachabilityStatus?.summary = CenoSettings.getReachabilityStatus(requireContext())
         preferenceLocalUdpEndpoint?.summary = CenoSettings.getLocalUdpEndpoint(requireContext()).ifNullOrEmpty { getString(R.string.not_applicable) }
         preferenceExternalUdpEndpoint?.summary = CenoSettings.getExternalUdpEndpoint(requireContext()).ifNullOrEmpty { getString(R.string.not_applicable) }
         preferencePublicUdpEndpoint?.summary = CenoSettings.getPublicUdpEndpoint(requireContext()).ifNullOrEmpty { getString(R.string.not_applicable) }
         preferenceUpnpStatus?.summary = CenoSettings.getUpnpStatus(requireContext())
-        preferenceExtraBitTorrentBootstrap?.summary = CenoSettings.getExtraBitTorrentBootstrap(requireContext())
+
+        val preferenceExtraBitTorrentBootstrap = findPreference<ListPreference>(extraBootstrapBittorrentKey)
+
+        preferenceExtraBitTorrentBootstrap?.entries = btSourcesMap.keys.toList().toTypedArray()
+        preferenceExtraBitTorrentBootstrap?.entryValues = btSourcesMap.values.toList().toTypedArray()
+        preferenceExtraBitTorrentBootstrap?.onPreferenceChangeListener = getClickListenerForExtraBitTorrentBootstraps()
+        preferenceExtraBitTorrentBootstrap?.summary = getBTPreferenceSummary()
 
     }
 
-    private fun getClickListenerForExtraBitTorrentBootstraps(): Preference.OnPreferenceClickListener {
-        return Preference.OnPreferenceClickListener {
+    private fun getClickListenerForExtraBitTorrentBootstraps(): Preference.OnPreferenceChangeListener {
+        return Preference.OnPreferenceChangeListener { _, newValue ->
+
             val context = requireContext()
             val dialogView = View.inflate(context, R.layout.extra_bittorrent_bootstrap_override_dialog, null)
             val bootstrapView = dialogView.findViewById<EditText>(R.id.bootstrap)
 
-            AlertDialog.Builder(context).apply {
-                setTitle(context.getString(R.string.customize_extra_bittorrent_bootstrap))
-                setView(dialogView)
-                setNegativeButton(R.string.customize_addon_collection_cancel) { dialog: DialogInterface, _ ->
-                    dialog.cancel()
+            when(newValue) {
+                "None" -> {
+                    setBTSource("")
                 }
+                "Custom" -> {
+                    AlertDialog.Builder(context).apply {
+                        setTitle(context.getString(R.string.customize_extra_bittorrent_bootstrap))
+                        setView(dialogView)
+                        setNegativeButton(R.string.customize_addon_collection_cancel) { dialog: DialogInterface, _ ->
+                            dialog.cancel()
+                        }
 
-                setPositiveButton(R.string.customize_add_bootstrap_save) { _, _ ->
+                        setPositiveButton(R.string.customize_add_bootstrap_save) { _, _ ->
+                            setBTSource(bootstrapView.text.toString().trim())
+                        }
 
-                    CenoSettings.ouinetClientRequest(
-                        requireContext(),
-                        OuinetKey.EXTRA_BOOTSTRAPS,
-                        OuinetValue.OTHER,
-                        bootstrapView.text.toString().trim()
-                    )
+                        setOnDismissListener {
+                            getPreference(R.string.pref_key_ouinet_extra_bittorrent_bootstraps)?.summary = getBTPreferenceSummary()
+                        }
 
-                    CenoSettings.setExtraBitTorrentBootstrap(
-                        context,
-                        arrayOf(bootstrapView.text.toString())
-                    )
-
-                    getPreference(R.string.pref_key_ouinet_extra_bittorrent_bootstraps)?.summary = CenoSettings.getExtraBitTorrentBootstrap(requireContext())
+                        bootstrapView.setText(
+                            CenoSettings.getExtraBitTorrentBootstrap(
+                                context
+                            )?.trim()
+                        )
+                        bootstrapView.requestFocus()
+                        bootstrapView.showKeyboard()
+                        create()
+                    }.show()
                 }
+                else -> {
+                    setBTSource(newValue as String)
+                }
+            }
 
-                bootstrapView.setText(
-                    CenoSettings.getExtraBitTorrentBootstrap(
-                        context
-                    )?.trim()
-                )
-                bootstrapView.requestFocus()
-                bootstrapView.showKeyboard()
-                create()
-            }.show()
+            getPreference(R.string.pref_key_ouinet_extra_bittorrent_bootstraps)?.summary = getBTPreferenceSummary()
+
             true
         }
     }
@@ -105,6 +119,36 @@ class NetworkSettingsFragment : PreferenceFragmentCompat() {
     private fun getPreference(key : Int) : Preference? {
         val prefKey = requireContext().getPreferenceKey(key)
         return findPreference(prefKey)
+    }
+
+    private fun setUpBTSources() {
+        for (entry in BuildConfig.BT_BOOTSTRAP_EXTRAS) btSourcesMap[entry[0]] = entry[1]
+        btSourcesMap["Custom"] = "Custom"
+        btSourcesMap["None"] = ""
+    }
+
+    private fun getBTPreferenceSummary(): String? {
+        val currentValue = CenoSettings.getExtraBitTorrentBootstrap(requireContext())?.trim()
+
+        return when {
+            currentValue == "" -> "None"
+            btSourcesMap.values.contains(currentValue) -> btSourcesMap.entries.find { it.value.trim() == currentValue }?.key
+            else -> currentValue
+        }
+    }
+
+    private fun setBTSource(source: String) {
+        CenoSettings.ouinetClientRequest(
+            requireContext(),
+            OuinetKey.EXTRA_BOOTSTRAPS,
+            OuinetValue.OTHER,
+            source
+        )
+
+        CenoSettings.setExtraBitTorrentBootstrap(
+            requireContext(),
+            arrayOf(source)
+        )
     }
 
     private fun getActionBar() = (activity as AppCompatActivity).supportActionBar!!
