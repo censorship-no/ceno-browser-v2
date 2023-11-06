a0a930617982481c39566837c3dc9df87e93c518
sphalt2 <sai@equalitie.org>
2023-11-06 12:23:14 -0500

    Fixes learn-more link on personal homepage

--- app/src/main/java/ie/equalit/ceno/home/personal/PersonalModeDescriptionViewHolder.kt
+++ app/src/main/java/ie/equalit/ceno/home/personal/PersonalModeDescriptionViewHolder.kt
@@ -1,8 +1,15 @@
 package ie.equalit.ceno.home.personal
 
+import android.annotation.SuppressLint
 import android.os.Build
 import android.text.Html
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.BackgroundColorSpan
+import android.text.style.URLSpan
+import android.view.MotionEvent
 import android.view.View
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import ie.equalit.ceno.R
 import ie.equalit.ceno.databinding.CenoModeItemBinding
@@ -20,6 +27,7 @@ class PersonalModeDescriptionViewHolder(
 
     init {
         binding.root.setBackgroundColor(ContextCompat.getColor(view.context ,R.color.fx_mobile_private_layer_color_1))
+        setLinkTextView(binding.tvPersonalBrowsingLearnMore, view.context.getString(R.string.personal_home_learn_more))
         binding.tvPersonalBrowsingLearnMore.setOnClickListener {
             interactor.onClicked(homepageCardType)
 
@@ -30,4 +38,36 @@ class PersonalModeDescriptionViewHolder(
     companion object {
         val homepageCardType = HomepageCardType.PERSONAL_MODE_CARD
     }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setLinkTextView (textView : TextView, text : String) {
+        val notClickedString = SpannableString(text)
+        notClickedString.setSpan(
+            URLSpan(""),
+            0,
+            notClickedString.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        textView.setText(notClickedString, TextView.BufferType.SPANNABLE)
+        val clickedString = SpannableString(notClickedString)
+        clickedString.setSpan(
+            BackgroundColorSpan(ContextCompat.getColor(textView.context, R.color.fx_mobile_text_color_secondary)), 0, notClickedString.length,
+            Spanned.SPAN_INCLUSIVE_EXCLUSIVE
+        )
+        textView.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> textView.text = clickedString
+                MotionEvent.ACTION_UP -> {
+                    textView.setText(notClickedString, TextView.BufferType.SPANNABLE)
+                    v.performClick()
+                }
+                MotionEvent.ACTION_CANCEL -> textView.setText(
+                    notClickedString,
+                    TextView.BufferType.SPANNABLE
+                )
+            }
+            true
+        }
+    }
+
 }
