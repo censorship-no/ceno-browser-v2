be1814796d042595513d0de6e13e8acd44fa105c
sphalt2 <sai@equalitie.org>
2023-12-13 15:37:00 -0500

    Adds set BT bootstrap extra dialog to standmy timeout dialog

--- app/src/main/java/ie/equalit/ceno/standby/StandbyFragment.kt
+++ app/src/main/java/ie/equalit/ceno/standby/StandbyFragment.kt
@@ -22,15 +22,18 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
+import ie.equalit.ceno.BuildConfig
 import ie.equalit.ceno.R
 import ie.equalit.ceno.databinding.FragmentStandbyBinding
 import ie.equalit.ceno.ext.requireComponents
+import ie.equalit.ceno.settings.ExtraBTBootstrapsDialog
 import ie.equalit.ouinet.Ouinet.RunningState
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 import mozilla.components.browser.state.selector.selectedTab
+import java.util.Locale
 
 
 /**
@@ -166,12 +169,26 @@ class StandbyFragment : Fragment() {
         }
 
         val dialog = timeoutDialogBuilder.create()
+
         val btnNetwork = timeoutDialogView.findViewById<Button>(R.id.btn_network_settings)
         btnNetwork?.setOnClickListener {
             isDialogVisible = false
             dialog.dismiss()
             val intent = Intent(ACTION_WIRELESS_SETTINGS)
             startActivity(intent)
+        }
+        val btnExtraBTBootstraps = timeoutDialogView.findViewById<Button>(R.id.btn_extra_bt_bootstraps)
+        btnExtraBTBootstraps.setOnClickListener{
+            dialog.dismiss()
+            val btSourcesMap = mutableMapOf<String, String>()
+            for (entry in BuildConfig.BT_BOOTSTRAP_EXTRAS) btSourcesMap[Locale("", entry[0]).displayCountry] = entry[1]
+            val extraBTDialog = ExtraBTBootstrapsDialog(requireContext(), btSourcesMap).getDialog()
+            extraBTDialog.setOnDismissListener {
+                isDialogVisible = false
+                tryAgain()
+            }
+            extraBTDialog.show()
+
         }
         dialog.show()
 
