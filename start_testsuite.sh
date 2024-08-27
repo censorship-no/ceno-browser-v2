ed24718fc399010cccb13b849f498bd38165fc49
paidforby <20746091+paidforby@users.noreply.github.com>
2024-08-27 17:03:13 -0400

    Default the toolbar position to top on Android 7.1 and earlier

--- start_testsuite.sh
+++ start_testsuite.sh
@@ -23,9 +23,9 @@ if [ $UPLOAD_APKS -eq 1 ]; then
   -F "custom_id=ouinet-debug-androidTest-latest"
 fi
 
-# "Samsung Galaxy S8-7.0", include after fixing https://gitlab.com/censorship-no/ceno-browser/-/issues/172
-DEVICES='["Samsung Galaxy S9-8.0", "Huawei P30-9.0", "Xiaomi Redmi Note 8-9.0", "Samsung Galaxy A51-10.0", "Xiaomi Redmi Note 11-11.0", "Google Pixel 6-12.0", "Samsung Galaxy S23-13.0", "Samsung Galaxy S24-14.0"]'
+DEVICES='["Samsung Galaxy S8-7.0", "Samsung Galaxy S9-8.0", "Huawei P30-9.0", "Xiaomi Redmi Note 8-9.0", "Samsung Galaxy A51-10.0", "Xiaomi Redmi Note 11-11.0", "Google Pixel 6-12.0", "Samsung Galaxy S23-13.0", "Samsung Galaxy S24-14.0"]'
 CONFIG='{"clearPackageData": "true", "deviceLogs": "true", "devices": '"${DEVICES}"', "app": "ouinet-debug-latest", "testSuite": "ouinet-debug-androidTest-latest" }'
+# To start only specific tests, add to `class` option to config, e.g. "class": ["ie.equalit.ceno.ui.ScreenshotGenerator"]
 
 curl -u "${BROWSERSTACK_USERNAME}:${BROWSERSTACK_ACCESS_KEY}" \
 -X POST "https://api-cloud.browserstack.com/app-automate/espresso/v2/build" \
